10f6c167325f964e7d53494570e15bbcfc85fb67
Ogushanakin <ogzhnakn96@gmail.com>
2023-01-10 13:12:54 +0300

    HomeScreen UI Design completed

--- WorkoutsFitness&Yoga/Screens/HomeScreen/HomeCell.swift
+++ WorkoutsFitness&Yoga/Screens/HomeScreen/HomeCell.swift
@@ -7,17 +7,77 @@
 
 import UIKit
 
-class HomeCell: UITableViewCell {
+final class HomeCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    // MARK: - Properties
+    
+    private let cellView: UIView = {
+        let view = UIView()
+        
+        let titleLabel = UILabel()
+        titleLabel.text = "Next Workouts"
+        titleLabel.textAlignment = .center
+        titleLabel.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        
+        view.addSubview(titleLabel)
+        titleLabel.anchor(top: view.topAnchor, left: view.leftAnchor)
+        
+        let seeAllLabel = UILabel()
+        seeAllLabel.text = "See All"
+        seeAllLabel.textAlignment = .center
+        seeAllLabel.textColor = .gray
+        seeAllLabel.font = UIFont.systemFont(ofSize: 13, weight: .light)
+        
+        view.addSubview(seeAllLabel)
+        seeAllLabel.anchor(top: view.topAnchor, right: view.rightAnchor)
+        
+        let posterImage = UIImageView()
+        posterImage.image = UIImage(named: "yoga")
+        posterImage.clipsToBounds = true
+        posterImage.contentMode = .scaleAspectFill
+        posterImage.layer.cornerRadius = 20
+        
+        view.addSubview(posterImage)
+        posterImage.anchor(top: titleLabel.bottomAnchor,
+                           left: view.safeAreaLayoutGuide.leftAnchor,
+                           bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                           right: view.safeAreaLayoutGuide.rightAnchor, paddingTop: 10, paddingBottom: 20)
+        
+        let posterLabel = UILabel()
+        posterLabel.text = "Strecthing"
+        posterLabel.textAlignment = .center
+        posterLabel.textColor = .white
+        posterLabel.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        
+        view.addSubview(posterLabel)
+        posterLabel.anchor(left: posterImage.leftAnchor, bottom: posterImage.bottomAnchor,
+                           paddingLeft: 14, paddingBottom: 14)
+        
+        return view
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    // MARK: - Helpers
+    
+    func configureUI() {
+        
+        backgroundColor = #colorLiteral(red: 0.9489933848, green: 0.9490416646, blue: 0.9454064965, alpha: 1)
+        
+        addSubview(cellView)
+        cellView.anchor(top: safeAreaLayoutGuide.topAnchor,
+                        left: safeAreaLayoutGuide.leftAnchor,
+                        bottom: safeAreaLayoutGuide.bottomAnchor,
+                        right: safeAreaLayoutGuide.rightAnchor,
+                        paddingLeft: 24, paddingRight: 24)
     }
-
 }
--- WorkoutsFitness&Yoga/Screens/HomeScreen/HomeController.swift
+++ WorkoutsFitness&Yoga/Screens/HomeScreen/HomeController.swift
@@ -13,14 +13,13 @@ final class HomeController: UITableViewController {
     
     // MARK: - Properties
     
-    private lazy var headerView = HomeHeaderView(frame: .init(x: 0, y: 0, width: view.frame.width, height: 500))
+    private lazy var headerView = HomeHeaderView(frame: .init(x: 0, y: 0, width: view.frame.width, height: 375))
     private let footerView = HomeFooterView()
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         configureUI()
     }
     
@@ -37,17 +36,36 @@ final class HomeController: UITableViewController {
         
         view.backgroundColor = #colorLiteral(red: 0.9489933848, green: 0.9490416646, blue: 0.9454064965, alpha: 1)
         
+        tableView.separatorStyle = .none
         tableView.tableHeaderView = headerView
         tableView.register(HomeCell.self, forCellReuseIdentifier: reuseIdentifier)
         tableView.tableFooterView = UIView()
-        tableView.contentInsetAdjustmentBehavior = .never
-        tableView.rowHeight = 64
+        tableView.rowHeight = 300
         tableView.backgroundColor = #colorLiteral(red: 0.9489933848, green: 0.9490416646, blue: 0.9454064965, alpha: 1)
         
         tableView.tableFooterView = footerView
         footerView.frame = .init(x: 0, y: 0, width: view.frame.width, height: 100)
     }
-    
-
 }
 
+// MARK: - UITableViewDataSource
+
+extension HomeController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->     UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! HomeCell
+        cell.selectionStyle = .none
+        return cell
+    }
+}
+
+ // MARK: - UITableViewDelegate
+
+extension HomeController {
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return UIView()
+    }
+}
